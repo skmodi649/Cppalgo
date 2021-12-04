7470afd9e4f8ea124e3b6929b1e26483dd2df47f
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2021-12-04 10:31:51 +0530

    jarvins in c update commit

--- Jarvins_algo.c
+++ Jarvins_algo.c
@@ -54,7 +54,7 @@ void convexHull(struct Point points[], int n)
 	} while (p != l); // While we don't come to first point
 
 	// Print Result
-	for (int i = 0; i < len-1; i++){
+	for (int i = 0; i < len; i++){
 		printf("%d %d",hull[i].x,hull[i].y);
 		printf("\n");
 	}
