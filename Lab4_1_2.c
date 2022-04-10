46efa07b074546ad3b0e782540d89de0902facc7
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2022-04-10 14:51:49 +0530

    bfs and dfs programs commit

--- Lab4_1_2.c
+++ Lab4_1_2.c
@@ -16,25 +16,25 @@ void bfs(int v)
 void main()
 {
  int v;
- printf("n Enter the number of vertices:");
+ printf("\n Enter the number of vertices:");
  scanf("%d",&n);
  for(i=1;i<=n;i++)
  {
   q[i]=0;
   visited[i]=0;
  }
- printf("n Enter graph data in matrix form:n");
+ printf("\n Enter graph data in matrix form : ");
  for(i=1;i<=n;i++)
   for(j=1;j<=n;j++)
    scanf("%d",&a[i][j]);
- printf("n Enter the starting vertex:");
+ printf("\n Enter the starting vertex:");
  scanf("%d",&v);
  bfs(v);
- printf("n The node which are reachable are:n");
+ printf("\n The node which are reachable are : ");
  for(i=1;i<=n;i++)
   if(visited[i])
-   printf("%dt",i);
+   printf("%d \t",i);
   else
-   printf("n Bfs is not possible");
+   printf("\n Bfs is not possible");
  getch();
 }
