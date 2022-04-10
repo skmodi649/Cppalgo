8bf6cb8f9c625c6089217c6b9bcf9ddb3623ac09
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2022-04-10 23:49:22 +0530

    sdsfsdfgdsfg

--- Lab4_4_Dijkstras_algorithm.c
+++ Lab4_4_Dijkstras_algorithm.c
@@ -58,58 +58,58 @@ int main() {
   n = 7;
 
   Graph[0][0] = 0;
-  Graph[0][1] = 0;
-  Graph[0][2] = 1;
-  Graph[0][3] = 2;
-  Graph[0][4] = 0;
-  Graph[0][5] = 0;
-  Graph[0][6] = 0;
+  Graph[0][1] = 3;
+  Graph[0][2] = 6;
+  Graph[0][3] = INFINITY;
+  Graph[0][4] = INFINITY;
+  Graph[0][5] = INFINITY;
+  Graph[0][6] = INFINITY;
 
-  Graph[1][0] = 0;
+  Graph[1][0] = 3;
   Graph[1][1] = 0;
   Graph[1][2] = 2;
-  Graph[1][3] = 0;
-  Graph[1][4] = 0;
-  Graph[1][5] = 3;
-  Graph[1][6] = 0;
+  Graph[1][3] = 4;
+  Graph[1][4] = INFINITY;
+  Graph[1][5] = INFINITY;
+  Graph[1][6] = INFINITY;
 
-  Graph[2][0] = 1;
+  Graph[2][0] = 6;
   Graph[2][1] = 2;
   Graph[2][2] = 0;
   Graph[2][3] = 1;
-  Graph[2][4] = 3;
-  Graph[2][5] = 0;
-  Graph[2][6] = 0;
+  Graph[2][4] = 4;
+  Graph[2][5] = 2;
+  Graph[2][6] = INFINITY;
 
-  Graph[3][0] = 2;
-  Graph[3][1] = 0;
+  Graph[3][0] = INFINITY;
+  Graph[3][1] = 4;
   Graph[3][2] = 1;
   Graph[3][3] = 0;
-  Graph[3][4] = 0;
+  Graph[3][4] = 2;
   Graph[3][5] = 0;
-  Graph[3][6] = 1;
+  Graph[3][6] = 4;
 
-  Graph[4][0] = 0;
-  Graph[4][1] = 0;
-  Graph[4][2] = 3;
-  Graph[4][3] = 0;
+  Graph[4][0] = INFINITY;
+  Graph[4][1] = INFINITY;
+  Graph[4][2] = INFINITY;
+  Graph[4][3] = 4;
   Graph[4][4] = 0;
   Graph[4][5] = 2;
-  Graph[4][6] = 0;
+  Graph[4][6] = 1;
 
-  Graph[5][0] = 0;
-  Graph[5][1] = 3;
-  Graph[5][2] = 0;
-  Graph[5][3] = 0;
+  Graph[5][0] = INFINITY;
+  Graph[5][1] = INFINITY;
+  Graph[5][2] = 2;
+  Graph[5][3] = INFINITY;
   Graph[5][4] = 2;
   Graph[5][5] = 0;
   Graph[5][6] = 1;
 
-  Graph[6][0] = 0;
-  Graph[6][1] = 0;
-  Graph[6][2] = 0;
-  Graph[6][3] = 1;
-  Graph[6][4] = 0;
+  Graph[6][0] = INFINITY;
+  Graph[6][1] = INFINITY;
+  Graph[6][2] = INFINITY;
+  Graph[6][3] = 4;
+  Graph[6][4] = 1;
   Graph[6][5] = 1;
   Graph[6][6] = 0;
 
